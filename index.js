57a64ff013e5f74c5ddde0023518a263ac378278
Gleb Svintsytskyi <globusmen0451@gmail.com>
2023-01-25 00:08:34 +0300

    finish task

--- index.js
+++ index.js
@@ -14,19 +14,22 @@ const team1 = [
 const specialization1 = {}
 
 const calculateTeamFinanceReport = (salaries, team, specialization) => {
-    let totalBudgetTeam = 0;
-    team.forEach(e => {
-        let res = salaries[e.specialization].salary * parseInt(salaries[e.specialization].tax) / 100;
-        totalBudgetTeam += salaries[e.specialization].salary + res;
-        let status = specialization.hasOwnProperty(e.specialization);
-        if (status) {
-            specialization[e.specialization] += salaries[e.specialization].salary + res;
-        } else {
-            specialization[e.specialization] =  salaries[e.specialization].salary + res;
-        }
+    if (team.length >= 1 && team.length <= 100 && Object.keys(salaries).length >= 1 && Object.keys(salaries).length <= 10) {
+        let totalBudgetTeam = 0;
+        team.forEach(e => {
+            let res = salaries[e.specialization].salary * parseInt(salaries[e.specialization].tax) / 100;
+            totalBudgetTeam += salaries[e.specialization].salary + res;
+            let status = specialization.hasOwnProperty(e.specialization);
+            if (status) {
+                specialization[e.specialization] += salaries[e.specialization].salary + res;
+            } else {
+                specialization[e.specialization] =  salaries[e.specialization].salary + res;
+            }
     });
-
-    return `totalBudgetTeam = ${totalBudgetTeam}, totalBudgetManager = ${specialization['Manager']}, totalBudgetDesigner = ${specialization['Designer']}, totalBudgetArtist = ${specialization['Artist']}`;
+        return `totalBudgetTeam = ${totalBudgetTeam}, totalBudgetManager = ${specialization['Manager']}, totalBudgetDesigner = ${specialization['Designer']}, totalBudgetArtist = ${specialization['Artist']}`;
+    } else {
+        console.error('minimum specializations amount is 1, maximum up to 10 and minimal team size is 1, maximum up to 100');
+    }
 }
 
 const financeReport1 = calculateTeamFinanceReport(salaries1, team1, specialization1);
